ba41197a1c64037c993282e8dddb64fe11bdbcad
RyanPPitts <ryanpitts26@gmail.com>
2019-12-05 20:27:23 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ import { reduxFirestore, getFirestore } from "redux-firestore";
 import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
 import fbConfig from "./config/fbConfig";
 
-// const store = createStore(
-//   rootReducer,
-//   compose(
-//     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-//     reduxFirestore(fbConfig),
-//     reactReduxFirebase(fbConfig, { attachAuthIsReady: true })
-//   )
-// );
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig, { attachAuthIsReady: true })
+  )
+);
 
 store.firebaseAuthisReady.then(() => {
   ReactDOM.render(
